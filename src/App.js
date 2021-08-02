b4a9336f8e256192d8cd7c748ea9de1bcb65ea33
Nahuel Savedra <nsavedra97@gmail.com>
2021-08-02 20:09:36 -0300

    project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from'react';
+import CenteredGrid from 'components/CenteredGrid.jsx';
+
+
+function App() {
+  return (
+    <div>
+      <CenteredGrid></CenteredGrid>
+      <h1>HOLA</h1>
+    </div>
+  );
+}
+
+export default App;
